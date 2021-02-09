ab4e7dd167d10c9f5eeef4e06b6439412fb47e3e
Vishalnarasimhan <77301922+Vishalnarasimhan@users.noreply.github.com>
2021-02-08 23:53:24 -0500

    Add files via upload

---
+++ project1/src/model/lab1.java
@@ -0,0 +1,313 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.swing.Icon;
+
+/**
+ *
+ * @author visha
+ */
+public class lab1 {
+    
+  private String fristname;
+  private String lastname;
+  private String Phone;
+  private String DOB;
+  private String Age;
+  private String Height;
+  private String Weight;
+  private String SSN;
+  private String streetaddress;
+  private String City;
+  private String state;
+  private String Zipcode;
+  private String SBankname;
+  private String Sbankrouatingnumber;
+  private String Sbankaccountnumber;
+  private String Saccountbalance;
+  private String Saccounttype;
+  private String CBankname;
+  private String Cbankrouatingnumber;
+  private String Cbankaccountnumber;
+  private String Caccountbalance;
+  private String Caccounttype;
+  private String Licensenumber;
+  private String issuedate;
+  private String expdate;
+  private String bloodtype;
+  private String Medicalnumber;
+  private String Alergy1;
+  private String Alergy2;
+  private String Alergy3;
+  private Icon Image;
+  private String ImagePath;
+
+    public String getImagePath() {
+        return ImagePath;
+    }
+
+    public void setImagePath(String ImagePath) {
+        this.ImagePath = ImagePath;
+    }
+ 
+
+    public Icon getImage() {
+        return Image;
+    }
+
+    public void setImage(Icon Image) {
+        this.Image = Image;
+    }
+
+    public String getStreetaddress() {
+        return streetaddress;
+    }
+
+    public void setStreetaddress(String streetaddress) {
+        this.streetaddress = streetaddress;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return Zipcode;
+    }
+
+    public void setZipcode(String Zipcode) {
+        this.Zipcode = Zipcode;
+    }
+
+    public String getSBankname() {
+        return SBankname;
+    }
+
+    public void setSBankname(String SBankname) {
+        this.SBankname = SBankname;
+    }
+
+    public String getSbankrouatingnumber() {
+        return Sbankrouatingnumber;
+    }
+
+    public void setSbankrouatingnumber(String Sbankrouatingnumber) {
+        this.Sbankrouatingnumber = Sbankrouatingnumber;
+    }
+
+    public String getSbankaccountnumber() {
+        return Sbankaccountnumber;
+    }
+
+    public void setSbankaccountnumber(String Sbankaccountnumber) {
+        this.Sbankaccountnumber = Sbankaccountnumber;
+    }
+
+    public String getSaccountbalance() {
+        return Saccountbalance;
+    }
+
+    public void setSaccountbalance(String Saccountbalance) {
+        this.Saccountbalance = Saccountbalance;
+    }
+
+    public String getSaccounttype() {
+        return Saccounttype;
+    }
+
+    public void setSaccounttype(String Saccounttype) {
+        this.Saccounttype = Saccounttype;
+    }
+
+    public String getCBankname() {
+        return CBankname;
+    }
+
+    public void setCBankname(String CBankname) {
+        this.CBankname = CBankname;
+    }
+
+    public String getCbankrouatingnumber() {
+        return Cbankrouatingnumber;
+    }
+
+    public void setCbankrouatingnumber(String Cbankrouatingnumber) {
+        this.Cbankrouatingnumber = Cbankrouatingnumber;
+    }
+
+    public String getCbankaccountnumber() {
+        return Cbankaccountnumber;
+    }
+
+    public void setCbankaccountnumber(String Cbankaccountnumber) {
+        this.Cbankaccountnumber = Cbankaccountnumber;
+    }
+
+    public String getCaccountbalance() {
+        return Caccountbalance;
+    }
+
+    public void setCaccountbalance(String Caccountbalance) {
+        this.Caccountbalance = Caccountbalance;
+    }
+
+    public String getCaccounttype() {
+        return Caccounttype;
+    }
+
+    public void setCaccounttype(String Caccounttype) {
+        this.Caccounttype = Caccounttype;
+    }
+
+    public String getLicensenumber() {
+        return Licensenumber;
+    }
+
+    public void setLicensenumber(String Licensenumber) {
+        this.Licensenumber = Licensenumber;
+    }
+
+    public String getIssuedate() {
+        return issuedate;
+    }
+
+    public void setIssuedate(String issuedate) {
+        this.issuedate = issuedate;
+    }
+
+    public String getExpdate() {
+        return expdate;
+    }
+
+    public void setExpdate(String expdate) {
+        this.expdate = expdate;
+    }
+
+    public String getBloodtype() {
+        return bloodtype;
+    }
+
+    public void setBloodtype(String bloodtype) {
+        this.bloodtype = bloodtype;
+    }
+
+    public String getMedicalnumber() {
+        return Medicalnumber;
+    }
+
+    public void setMedicalnumber(String Medicalnumber) {
+        this.Medicalnumber = Medicalnumber;
+    }
+
+    public String getAlergy1() {
+        return Alergy1;
+    }
+
+    public void setAlergy1(String Alergy1) {
+        this.Alergy1 = Alergy1;
+    }
+
+    public String getAlergy2() {
+        return Alergy2;
+    }
+
+    public void setAlergy2(String Alergy2) {
+        this.Alergy2 = Alergy2;
+    }
+
+    public String getAlergy3() {
+        return Alergy3;
+    }
+
+    public void setAlergy3(String Alergy3) {
+        this.Alergy3 = Alergy3;
+    }
+          
+  
+  
+  
+  
+  
+
+    public String getFristname() {
+        return fristname;
+    }
+
+    public void setFristname(String fristname) {
+        this.fristname = fristname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getHeight() {
+        return Height;
+    }
+
+    public void setHeight(String Height) {
+        this.Height = Height;
+    }
+
+    public String getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(String Weight) {
+        this.Weight = Weight;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+  
+  
+}
